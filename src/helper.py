8c5d500a0531099cab848d59044ce96e4e11193a
AplhaSlayer1964 <62714929+AplhaSlayer1964@users.noreply.github.com>
2021-10-26 16:06:08 -0400

    Update helper.py

--- src/helper.py
+++ src/helper.py
@@ -11,28 +11,28 @@ def check_post_archived(post):
         if os.path.exists(args['archive']):
             with open(args['archive'],'r') as f:
                 archived = f.read().splitlines()
-                
+
             if '/{service}/user/{user}/post/{id}'.format(**post) in archived:
-                return False          
+                return False
     return True
 
 
 def check_date(date):
     if not args['date'] and not args['datebefore'] and not args['dateafter']:
         return True
-    
+
     if date == datetime.datetime.min:
         return False
-    
+
     if not args['datebefore']:
         args['datebefore'] = datetime.datetime.min
-        
+
     if not args['dateafter']:
         args['dateafter'] = datetime.datetime.max
-        
+
     if not args['date']:
         args['date'] = datetime.datetime.min
-        
+
     if date == args['date'] or date <= args['datebefore'] or date >= args['dateafter']:
         return True
     return False
@@ -41,23 +41,23 @@ def check_date(date):
 def check_size(size):
     if not args['min_filesize'] and not args['max_filesize']:
         return True
-    
+
     if size == 0:
         return False
-    
+
     if not args['min_filesize']:
         args['min_filesize'] = '0'
-        
+
     if not args['max_filesize']:
         args['max_filesize'] = 'inf'
-        
+
     if int(size) <= float(args['max_filesize']) and int(size) >= int(args['min_filesize']):
-        return True 
-    return False 
+        return True
+    return False
 
 def check_extention(file_name):
     file_extention = file_name.split('.')[-1]
-    
+
     for valid_extention in args['only_filetypes']:
         if valid_extention == file_extention.lower():
             return True
@@ -69,7 +69,7 @@ def check_extention(file_name):
             print('Skiping "{}"'.format(file_name))
             return False
         return True
-    
+
     return True
 
 
@@ -83,7 +83,7 @@ def win_file_name(file_name):
     else:
         file_name[0] = file_name[0][:260] # if file name some how has no extention
         return file_name[0]
-    
+
 def win_folder_name(folder_name):
     folder_name = re.sub(r'[\n\t]+',' ', folder_name) # convert newline and tabs to white space
     folder_name = re.sub(r'[\\/:\"*?<>|]+','', folder_name) # remove illgal file name characters
@@ -97,5 +97,5 @@ def add_indexing(index, file_name, list):
         return '[{:01d}]_{}'.format(index+1, file_name)
     elif len(list) < 100:
         return '[{:02d}]_{}'.format(index+1, file_name)
-    elif len(list) < 1000: # there is no way a post has more than 1000 attachments! 
-        return '[{:03d}]_{}'.format(index+1, file_name) 
+    elif len(list) < 1000: # there is no way a post has more than 1000 attachments!
+        return '[{:03d}]_{}'.format(index+1, file_name)
