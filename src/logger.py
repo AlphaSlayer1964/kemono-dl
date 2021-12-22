9954ff0acc577497effb955ff6423327ae68243c
AplhaSlayer1964 <62714929+AplhaSlayer1964@users.noreply.github.com>
2021-12-21 22:20:28 -0600

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -4,9 +4,10 @@ from .arguments import get_args
 
 args = get_args()
 
-# clear log file
-file = open('debug.log','w')
-file.close()
+if args['verbose']:
+    # clear log file
+    file = open('debug.log','w')
+    file.close()
 
 logging.getLogger("requests").setLevel(logging.WARNING)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
@@ -28,5 +29,6 @@ file_handler.setFormatter(file_format)
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(stream_format)
 
-logger.addHandler(file_handler)
+if args['verbose']:
+    logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
