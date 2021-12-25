70632c331684fad9a4d31a2a4480073aafa2dedd
AplhaSlayer1964 <62714929+AplhaSlayer1964@users.noreply.github.com>
2021-12-25 02:42:00 -0600

    Update arguments.py

--- src/arguments.py
+++ src/arguments.py
@@ -18,25 +18,29 @@ def get_args():
                     action='store_true', default=False,
                     help="Display extra debug information and create a debug.log")
 
+    ap.add_argument("--quiet",
+                    action='store_true', default=False,
+                    help="Suppress printing except for warnings, errors, and critical messages")
+
     ap.add_argument("--cookies",
                     metavar="FILE", type=str, default=None,
-                    help="Files to read cookies from, comma separated. (REQUIRED)")
+                    help="Files to read cookies from, can take a comma seperated list. (REQUIRED)")
 
-    ap.add_argument("--links",
-                    default=[],
-                    help="Downloads URLs, can be separated by commas.")
+    ap.add_argument("-l","--links",
+                    metavar="LINKS", type=str, default=[],
+                    help="URLs to be downloaded, can take a comma seperated list.")
 
-    ap.add_argument("--fromfile",
+    ap.add_argument("-f","--fromfile",
                     metavar="FILE", type=str, default=[],
-                    help="File containing URLs to download, one URL per line. Lines starting with a '#' don't count")
+                    help="File containing URLs to download, one URL per line. Lines starting with a \"#\" are counted as comments")
 
     ap.add_argument("--kemono-favorite-users",
                     action='store_true', default=False,
-                    help="Downloads all favorite users from kemono.party. (Requires cookies while logged in)")
+                    help="Adds all favorite users posts from kemono.party. (Requires cookies while logged in)")
 
     ap.add_argument("--coomer-favorite-users",
                     action='store_true', default=False,
-                    help="Downloads all favorite users from coomer.party. (Requires cookies while logged in)")
+                    help="Adds all favorite users posts from coomer.party. (Requires cookies while logged in)")
 
     ap.add_argument("--favorite-users-updated-within",
                     metavar="N_DAYS", type=int, default=0,
@@ -44,17 +48,25 @@ def get_args():
 
     ap.add_argument("--kemono-favorite-posts",
                     action='store_true', default=False,
-                    help="Downloads all favorites posts from kemono.party. (Requires cookies while logged in)")
+                    help="Adds all favorites posts from kemono.party. (Requires cookies while logged in)")
 
     ap.add_argument("--coomer-favorite-posts",
                     action='store_true', default=False,
-                    help="Downloads all favorites posts from coomer.party. (Requires cookies while logged in)")
+                    help="Adds all favorites posts from coomer.party. (Requires cookies while logged in)")
 
-    ap.add_argument("--output",
+    ap.add_argument("-o","--output",
                     metavar="PATH", type=str, default=None,
                     help="Path to download location")
 
-    ap.add_argument("--archive",
+    ap.add_argument("--restrict-names",
+                    action='store_true', default=False,
+                    help='Restrict filenames and foldernames to only ASCII characters, and remove "&" and spaces')
+
+    ap.add_argument("--no-indexing",
+                    action='store_true', default=False,
+                    help="Do not index file names. Might cause issues if attachments have duplicate names.")
+
+    ap.add_argument("-a","--archive",
                     metavar="FILE", type=str, default=None,
                     help="Downloads only posts that are not in provided archive file. (Can not be used with --update-posts)")
 
@@ -72,7 +84,7 @@ def get_args():
 
     ap.add_argument("--retry-download",
                     metavar="COUNT", type=int, default=5,
-                    help="The amount of times to retry downloading a file. (default: 5)")
+                    help="The amount of times to retry / resume downloading a file. (default: 5)")
 
     ap.add_argument("--date",
                     metavar="YYYYMMDD", type=str, default=None,
@@ -96,23 +108,19 @@ def get_args():
 
     ap.add_argument("--only-filetypes",
                     metavar="EXT", type=str, default=[],
-                    help="Only downloads attachments and post file with given EXTs, can be separated by commas. (ex. JPG,mp4,mp3,png)")
+                    help="Only downloads files with the given extention(s), can take a comma seperated list. (ex. \"JPG,mp4,mp3,png\")")
 
     ap.add_argument("--skip-filetypes",
                     metavar="EXT", type=str, default=[],
-                    help="Skips attachments and post file with given EXTs, can be separated by commas. (ex. JPG,mp4,mp3,png)")
-
-    ap.add_argument("--simulate",
-                    action='store_true', default=False,
-                    help="Simulate Downloads")
+                    help="Skips files with the given extention(s), can take a comma seperated list. (ex. \"JPG,mp4,mp3,png\")")
 
     ap.add_argument("--skip-content",
                     action='store_true', default=False,
                     help="Skips posts content.")
 
-    ap.add_argument("--skip-embeds",
+    ap.add_argument("--extract-links",
                     action='store_true', default=False,
-                    help="Skips posts embeds. Also skips downloading post embeds using --yt-dlp")
+                    help="Save all content links to a file.")
 
     ap.add_argument("--skip-comments",
                     action='store_true', default=False,
@@ -122,21 +130,13 @@ def get_args():
                     action='store_true', default=False,
                     help="Skips attachments.")
 
+    ap.add_argument("--skip-embeds",
+                    action='store_true', default=False,
+                    help="Skips posts embeds. Also skips downloading post embeds using --yt-dlp")
+
     ap.add_argument("--skip-json",
                     action='store_true', default=False,
                     help="Skips json. (--update-posts requires post json)")
-
-    ap.add_argument("--extract-links",
-                    action='store_true', default=False,
-                    help="Save all content links to a file.")
-
-    ap.add_argument("--no-indexing",
-                    action='store_true', default=False,
-                    help="Do not index file names. Might cause issues if attachments have duplicate names.")
-
-    ap.add_argument("--quiet",
-                    action='store_true', default=False,
-                    help="Suppress printing except for warnings, errors, and critical messages")
 
     ap.add_argument("--save-pfp",
                     action='store_true', default=False,
@@ -146,9 +146,10 @@ def get_args():
                     action='store_true', default=False,
                     help="Downloads user banner")
 
-    ap.add_argument("--restrict-names",
+    # add more simulation options
+    ap.add_argument("--simulate",
                     action='store_true', default=False,
-                    help='Restrict filenames and foldernames to only ASCII characters, and remove "&" and spaces')
+                    help="Simulate Downloads")
 
     ap.add_argument("--user-agent",
                     metavar="UA", type=str, default='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
@@ -162,58 +163,34 @@ def get_args():
     # --part
     # --no-part
 
-    # renamed
-    ap.add_argument("--force-external",
-                    action='store_true', default=False,
-                    help="RENAMED changed name to better fit action, --extract-links")
     # deprecated
-    ap.add_argument("-i", "--ignore-errors",
-                    action='store_true', default=False,
-                    help="DEPROCATED 404 errors are skipped by default and 429 cause a 5 minute waite time. If you get any other errors they probably shouldn\'t be ignored")
-    ap.add_argument("--skip-postfile",
-                    action='store_true', default=False,
-                    help="DEPROCATED post file is merged with attachments")
-    ap.add_argument("--force-indexing",
-                    action='store_true', default=False,
-                    help="DEPROCATED files are indexed by default")
-    ap.add_argument("--force-inline",
-                    action='store_true', default=False,
-                    help="DEPROCATED causes to many issues. Images not saved by party sites should still show up in content.html")
-    ap.add_argument("--force-yt-dlp",
-                    action='store_true', default=False,
-                    help="DEPROCATED I found there were too many incorrect links causing issues")
-    ap.add_argument("--favorite-users",
-                    action='store_true', default=False,
-                    help="DEPROCATED use --kemono-favorite-users or --coomer-favorite-users")
-    ap.add_argument("--favorite-posts",
-                    action='store_true', default=False,
-                    help="DEPROCATED use --kemono-favorite-posts or --coomer-favorite-posts")
-    ap.add_argument("--skip-pfp-banner",
-                    action='store_true', default=False,
-                    help="DEPROCATED decided not to download pfp or banner by default")
+    deprecated_list = (
+        "--force-external",
+        "--ignore-errors",
+        "--skip-postfile",
+        "--force-indexing",
+        "--force-inline",
+        "--force-yt-dlp",
+        "--favorite-users",
+        "--favorite-posts",
+        "--skip-pfp-banner"
+    )
+    ap.add_argument("--force-external",action='store_true',default=False,help="DEPROCATED")
+    ap.add_argument("-i", "--ignore-errors",action='store_true',default=False,help="DEPROCATED")
+    ap.add_argument("--skip-postfile",action='store_true',default=False,help="DEPROCATED")
+    ap.add_argument("--force-indexing",action='store_true',default=False,help="DEPROCATED")
+    ap.add_argument("--force-inline",action='store_true',default=False,help="DEPROCATED")
+    ap.add_argument("--force-yt-dlp",action='store_true',default=False,help="DEPROCATED")
+    ap.add_argument("--favorite-users",action='store_true',default=False,help="DEPROCATED")
+    ap.add_argument("--favorite-posts",action='store_true',default=False,help="DEPROCATED")
+    ap.add_argument("--skip-pfp-banner",action='store_true',default=False,help="DEPROCATED")
 
     args = vars(ap.parse_args())
 
-    # deprocated
-    if args['favorite_users']:
-        print('--favorite-users: DEPROCATED use --kemono-favorite-users or --coomer-favorite-users')
-    if args['favorite_posts']:
-        print('--favorite-posts: DEPROCATED use --kemono-favorite-posts or --coomer-favorite-posts')
-    if args['force_yt_dlp']:
-        print('--force-yt-dlp: DEPROCATED I found there were too many incorrect links causing issues')
-    if args['force_inline']:
-        print('--force-inline: DEPROCATED causes to many issues. Images not saved by party sites should still show up in content.html')
-    if args['ignore_errors']:
-        print('--ignore-errors: DEPROCATED 404 errors are skipped by default and 429 cause a 5 minute waite time. If you get any other errors they probably shouldn\'t be ignored')
-    if args['skip_postfile']:
-        print('--skip-postfile: DEPROCATED post files is merged with attachments')
-    if args['force_indexing']:
-        print('--force-indexing: DEPROCATED files are indexed by default')
-    if args['skip_pfp_banner']:
-        print('--skip-pfp-banner: DEPROCATED decided not to download pfp or banner by default')
-    # renamed
-    if args['force_external']:
-        print('--force-external: RENAMED to --extract-links : changed name to better fit action')
+    # print deprocated arguments
+    for key, value in args.items():
+        if key in deprecated_list and value:
+            print(f"The argument \"{key}\" is DEPROCATED and will be ignored")
 
     if args['version']:
         print(__version__)
@@ -223,19 +200,13 @@ def get_args():
         print('--archive, --update-posts: Only use one at a time')
         quit()
 
-    # takes a list of cookie files and marges than and makes them usable by requests
+    # takes a list of cookie files and loads them all into a cookie jar
     if args['cookies']:
         cookie_files = args['cookies'].split(',')
         try:
-            if len(cookie_files) == 1:
-                args['cookies'] = MozillaCookieJar(args['cookies'])
-                args['cookies'].load()
-            elif len(cookie_files) == 2:
-                args['cookies'] = MozillaCookieJar()
-                args['cookies'].load(cookie_files[0])
-                args['cookies'].load(cookie_files[1])
-            else:
-                print('--cookies: You should only be passing in two cookie files, one for kemono.party and one for coomer.party')
+            args['cookies'] = MozillaCookieJar()
+            for cookie_file in cookie_files:
+                args['cookies'].load(cookie_file)
         except (LoadError, FileNotFoundError) as e:
             print(e)
             quit()
