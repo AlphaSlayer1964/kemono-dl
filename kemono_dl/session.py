c36b1b6d5fbf641d1b47ac0574413b8cad4d531c
AlphaSlayer1964 <62714929+AlphaSlayer1964@users.noreply.github.com>
2025-08-13 20:34:44 -0500

    stuff
    added template variables for `added`, `published`, and `edited`
    
    added missing Creator model values
    
    fixed encoding issue because kemono wants to say the api json is a `text/css` for some reason
    
    moved pytest to it's own requirements file

--- kemono_dl/session.py
+++ kemono_dl/session.py
@@ -8,4 +8,8 @@ class CustomSession(requests.Session):
         response = super().request(method, url, *args, **kwargs)
         if response.status_code == 403:
             raise DDOSGuardError(url)
+        content_type = response.headers["Content-Type"]
+        # why is the api content type text/css and not application/json!
+        if content_type == "text/css":
+            response.encoding = "UTF-8"
         return response
