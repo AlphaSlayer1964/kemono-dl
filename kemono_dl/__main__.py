78ad62ef5c5ed3da47c8663daa7b515aab8ac980
AlphaSlayer1964 <62714929+AlphaSlayer1964@users.noreply.github.com>
2025-08-14 21:37:43 -0500

    Update __main__.py

--- kemono_dl/__main__.py
+++ kemono_dl/__main__.py
@@ -17,13 +17,24 @@ def parse_args():
     parser.add_argument("--path", type=str, default=os.path.join(os.getcwd(), "Downloads"), help="Download directory path")
     parser.add_argument("--output", type=str, default="{service}/{creator_id}/{server_filename}", help="Post attachments output filename tamplate")
     # parser.add_argument("--output-special", type=str, default="{service}/{creator_id}/{type}_{sha256}.{file_ext}", help="Creator profile picture and banner output filename tamplate")
-    parser.add_argument("--coomer-cookies", type=str, default="coomer.st_cookies.txt", help="Path to Coomer cookies file")
-    parser.add_argument("--kemono-cookies", type=str, default="kemono.cr_cookies.txt", help="Path to Kemono cookies file")
+    parser.add_argument("--cookies", type=str, nargs="+", help="Path(s) to cookies files")
     parser.add_argument("--favorite-creators-coomer", action="store_true", help="Download favorite creators from Coomer")
     parser.add_argument("--favorite-creators-kemono", action="store_true", help="Download favorite creators from Kemono")
     # parser.add_argument("--favorite-posts-coomer", action="store_true", help="Download favorite posts from Coomer")
     # parser.add_argument("--favorite-posts-kemono", action="store_true", help="Download favorite posts from Kemono")
     parser.add_argument("--batch-file", type=str, help="Download URLs from a file")
+    parser.add_argument(
+        "--coomer-login",
+        nargs=2,
+        metavar=("USERNAME", "PASSWORD"),
+        help="Login for Coomer",
+    )
+    parser.add_argument(
+        "--kemono-login",
+        nargs=2,
+        metavar=("USERNAME", "PASSWORD"),
+        help="Login for Kemono",
+    )
     parser.add_argument("urls", nargs="*", help="URLs to download")
 
     return parser.parse_args()
@@ -34,11 +45,17 @@ def main() -> None:
 
     kemono_dl = KemonoDL(path=args.path, output_template=args.output)
 
-    if args.favorite_creators_coomer:
-        kemono_dl.load_cookies(args.coomer_cookies)
+    if args.cookies:
+        for cookie_file in args.cookies:
+            kemono_dl.load_cookies(cookie_file)
 
-    if args.favorite_creators_kemono:
-        kemono_dl.load_cookies(args.kemono_cookies)
+    if args.coomer_login:
+        kemono_dl.login(KemonoDL.COOMER_DOMAIN, args.coomer_login[0], args.coomer_login[1])
+        print(kemono_dl.isLoggedin(KemonoDL.COOMER_DOMAIN))
+
+    if args.kemono_login:
+        kemono_dl.login(KemonoDL.KEMONO_DOMAIN, args.kemono_login[0], args.kemono_login[1])
+        print(kemono_dl.isLoggedin(KemonoDL.KEMONO_DOMAIN))
 
     if args.favorite_creators_coomer:
         kemono_dl.download_favorite_creators(KemonoDL.COOMER_DOMAIN)
